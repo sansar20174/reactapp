4c9d507d670b3ce004c29945ba57b687ea74fd8b
Sansar Kumar <kumarsansar999@gmail.com>
2026-04-13 12:42:26 +0530

    feat: add styled-components and update dependencies
    - Added styled-components to package.json and package-lock.json.
    - Updated @emnapi/core, @emnapi/runtime, and @emnapi/wasi-threads to latest versions.
    - Added new dependencies: @emotion/is-prop-valid, @emotion/memoize, camelize, css-color-keywords, css-to-react-native, postcss-value-parser.
    - Updated existing dependencies: brace-expansion, picomatch, vite.
    - Refactored Article component to use styled-components for button styling.
    - Created new CSS modules for Article and HomePage components.
    - Added utility functions for addition and subtraction in utility.js.

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,72 +1,92 @@
-import React from "react";
-
+import React, {useState} from 'react'
+import style from "../css/Article.module.css"
+import style1 from "../css/HomePage.module.css"
+// import styled from "styled-components"
+import styled from "@emotion/styled"
+import { css } from '@emotion/react'
 function Article(props) {
-    const [inputValue, setInputValue] = React.useState(0);
-    const [count, setCount] = React.useState(0);
+  const [count, setCount]=useState(0)
 
-    const handleClick = () => {
-        setCount(count + 1);
-    }
-    const handleClick2 = () => {
-        setCount(count - 1);
-    }
-    const handleClick3 = () => {
-        setCount(0);
-    }
-    const handleClick4 = () => {
-        setCount(count * inputValue);
-    }
-    return (
-        
-        <div >
-            <h1>Article</h1>
-            <h3>Title: {props.title}</h3>
-            <p>This is count: </p>
-            <h2>{count}</h2>
-            <button onClick={handleClick}>Increament</button>
-            <br/>
-            <button onClick={handleClick2}>Decreament</button>
-            <br/>
-            <button onClick={handleClick3}>Reset</button>
-            <br/>
-            <input type="number" value={inputValue} onChange={(e) => setInputValue(Number(e.target.value))} />
-            <button onClick={handleClick4}>Double</button>
-        </div>
-    )
+
+  const handleIncrement=()=>{
+    setCount(count+1)
+  }
+  return (
+    <div>
+      <h1>Article</h1>
+       <h3>{props.title}</h3> 
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+      <h2>{count}</h2>
+      <button className={style.btn} onClick={handleIncrement}>Increment</button>
+      <button css={{backgroundColor:"green", color:"white"}}> First</button>
+      <Button>Submit</Button>
+      <Button yellow >Click Me!</Button>
+    </div>
+  )
 }
 
-export default Article;
+export default Article
 
-// import React, {Component} from "react";
+const Button=styled.button`
+background-color: ${props=>props.yellow ? "yellow" : "red"};  
+color: white;
+width: 100px;
+height: 30px;
+`
 
-// class Article extends Component {
+// import React from 'react'
+// import { add as addition } from '../utility'
 
+// class Article extends React.Component {
 //     constructor(props) {
-//         super(props);
-//         this.state = {
-//             count: 0
-//         };
+//       super(props)
+//       this.state={
+//         count:0,
+//         name:"Alex"
+//       }
+//     //   console.log(addition(2,3))
 //     }
 
-//     handleClick = () => {
-//         this.setState({ count: this.state.count + 1 })
-//     }
-//     handleClick2 = () => {
-//         this.setState({ count: this.state.count - 1 })
+//     componentDidMount(){
+//         console.log("componentDidMount")
 //     }
 
-//     render() {
-//         return (
-//             <div >
-//                 <h1>Article</h1>
-//                 <h3>Count: {this.state.count}</h3>
-//                 <h3>Title: {this.props.title}</h3>
-//                 <p>lorem ipsum dolor sit amet</p>
-//                 <button onClick={this.handleClick}>Increament</button>
-//                 <br/>
-//                 <button onClick={this.handleClick2}>Decreament</button>
-//             </div>
-//         )
+//     shouldComponentUpdate(nextProps, nextState){
+//       if(nextState.count===1)return false
+//         console.log("shouldComponentUpdate")
+//         return true
 //     }
+
+
+//     componentDidUpdate(){
+//         console.log("componentDidUpdate")
+//     }
+
+//     componentWillUnmount(){
+//         console.log("componentWillUnmount")
+//     }
+
+//     handleClick=()=>{
+//         // this.state.count=this.state.count+1
+//         // console.log(this.state.count)
+//         this.setState({count: this.state.count+1})
+//     }
+
+//     handleDecrement=()=>{
+//         this.setState({count: this.state.count-1})
+//     }
+//   render() {
+//     return (
+//       <div>
+//         <h1>Article</h1>
+//         <h3>{this.state.count}</h3>
+//         <button onClick={this.handleClick}>Increment</button>
+//         <button onClick={this.handleDecrement}>Decrement</button>
+//         <h3>{this.props.title}</h3> 
+//         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi eligendi fugiat pariatur eos quibusdam ratione possimus adipisci, impedit, autem ad quo, et cum laudantium deleniti. Soluta molestias amet quas labore.</p>
+//       </div>
+//     )
+//   }
 // }
-// export default Article;
+
+// export default  Article
